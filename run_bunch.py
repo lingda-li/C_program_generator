7ab2f2477a6ed7c73e4b93fdd3904c02c4940f3a
Lingda Li <lildmh@gmail.com>
2023-10-13 21:28:56 +0000

    Add timed run without perf

--- run_bunch.py
+++ run_bunch.py
@@ -1,6 +1,8 @@
 import sys
 import subprocess
 
+is_perf = True
+#is_perf = False
 assert len(sys.argv) > 1
 if len(sys.argv) > 2:
   start = int(sys.argv[2])
@@ -9,11 +11,18 @@ else:
 nerrs = 0
 for i in range(start, int(sys.argv[1])):
   filename = "./test" + str(i) + ""
-  cmd = ['perf', 'stat', '-e', 'cycles,cycles:u,instructions:u,cache-misses:u,cache-references:u,stalled-cycles-backend:u,stalled-cycles-frontend:u', '-r', '10']
-  cmd.append(filename)
+  if is_perf:
+    cmd = ['perf', 'stat', '-e', 'cycles,cycles:u,instructions:u,cache-misses:u,cache-references:u,stalled-cycles-backend:u,stalled-cycles-frontend:u', '-r', '10']
+    cmd.append(filename)
+  else:
+    cmd = "time " + filename
   #print(cmd)
   try:
-    subprocess.check_call(cmd)
+    if is_perf:
+      subprocess.run(cmd, check=True)
+    else:
+      for _ in range(10):
+        subprocess.run(cmd, check=True, shell=True, executable='/bin/bash')
   except subprocess.CalledProcessError:
     print("Error when running", filename)
     nerrs += 1
