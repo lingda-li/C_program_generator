2121569da47a99403a556da93a3076ec52a6d446
Lingda Li <lildmh@gmail.com>
2023-11-07 13:33:02 -0800

    Fix time log processing

--- process_log_time.py
+++ process_log_time.py
@@ -3,19 +3,24 @@ import subprocess
 import torch
 
 assert len(sys.argv) > 1
-nerrs = 0
+out_name = "all.pt"
 repeat_time = 0
 idx = 0
 step = 0
 internal_time = []
 real_time = []
-arr = torch.tensor(1000, 2, 10)
+end = 10000
+arr = torch.zeros(end, 2, 10)
+#torch.set_printoptions(precision=10)
 with open(sys.argv[1]) as f:
   for line in f:
     words = line.split()
     if step == 0:
-      assert len(words) == 2
-      cur_idx = int(words[0])
+      if len(words) != 2:
+        assert idx == end - 1
+        cur_idx = end
+      else:
+        cur_idx = int(words[0])
       if cur_idx != idx:
         assert cur_idx == idx + 1
         if repeat_time == 0:
@@ -23,7 +28,10 @@ with open(sys.argv[1]) as f:
         assert repeat_time == len(internal_time)
         arr[idx, 0] = torch.tensor(internal_time)
         arr[idx, 1] = torch.tensor(real_time)
+        #print(arr[idx])
         idx = cur_idx
+        if cur_idx == end:
+          break
         internal_time = []
         real_time = []
     elif step == 1:
@@ -46,5 +54,9 @@ with open(sys.argv[1]) as f:
         assert words[0] == "sys"
     step += 1
     step %= 6
-
-print("There were %d errors in total." % nerrs)
+torch.save(arr, out_name)
+mean = torch.mean(arr, dim=2)
+torch.save(mean[:, 0], "internal_mean.pt")
+std = torch.std(arr, dim=2)
+print(std)
+print("Internal and real mean stds:", torch.mean(std[:, 0]), torch.mean(std[:, 1]))
